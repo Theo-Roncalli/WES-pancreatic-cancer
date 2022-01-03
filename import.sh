7a1afb92fc7694d8f619733189d03f38572b22c8
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-03 13:25:43 +0100

    :bug: Exit when reads numbers are not the same between paired files

--- import.sh
+++ import.sh
@@ -8,6 +8,12 @@ reads=Data/Reads
 
 reads_url="'https://docs.google.com/uc?export=download&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe'"
 
+# Colors
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 # Step 1: Download reads
 
 mkdir -p ${reads}
@@ -19,11 +25,25 @@ tar -zxf ${reads}/patient7.tar.gz -C ${reads}
 mv ${reads}/patient7.exome/* ${reads} && rm -r ${reads}/patient7.exome && rm ${reads}/patient7.tar.gz # Cleaning 'reads' repository
 gunzip -q ${reads}/*.fastq.gz
 
-# check file sizes and estimate read numbers for each sample
-
+# Print read numbers and file sizes for each sample
 echo "--------Number of reads and size of each file--------"
 for file in ${reads}/*.fastq
 do
+    # Print filename, number of reads and size of a specific file
 	grep ^+$ ${file} | echo "$(wc -l) $(du -h ${file})" | awk '{print $3 " Reads:" $1 " Size:" $2}';
 done
 
+# Check if reads numbers are the same between paired files
+# Exit if not the case
+echo -e "\n"
+for read1_file in ${reads}/*_r1F.fastq
+do
+	paired_file_with_path=${read1_file%_r1F.fastq};
+    if [ $(grep ^+$ ${paired_file_with_path}_r1F.fastq | wc -l) == $(grep ^+$ ${paired_file_with_path}_r2F.fastq | wc -l) ]
+    then
+        echo -e "${GREEN}Number of reads between the paired files ${paired_file_with_path}_r1F.fastq and ${paired_file_with_path}_r2F.fastq are the same."
+    else
+        echo -e "${RED}Error: number of reads between the paired files ${paired_file_with_path}_r1F.fastq and ${paired_file_with_path}_r2F.fastq are not the same."
+        exit 1;
+    fi
+done
