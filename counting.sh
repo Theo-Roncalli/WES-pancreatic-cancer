d5a4511f095a9b43550e70f5d5111fb60cf1a5ac
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-03 18:33:31 +0100

    :sparkles: Added mapping

--- counting.sh
+++ counting.sh
@@ -2,46 +2,43 @@
 
 # Directory parameters
 
-reads=Data/Reads
+# reads=Data/Reads
 trimming=Data/Trimming
-genome=Data/Genome
 index=Data/Index
+# mapping=Data/Mapping
+# counts=Data/Counts
 mapping=Data/Mapping
-counts=Data/Counts
-figures_reads=Figures/Reads
-figures_trimming=Figures/Trimming
 
 # Performance parameters
 
-nb_cpus_indexing=7
 nb_cpus_mapping=7
-nb_cpus_counting=7
 
-# Step 1: Quality control + Reads cleaning
+# Color parameters
 
-mkdir -p ${figures_reads}
-echo "Creation of the fastqc files on raw reads."
-fastqc -o ${figures_reads} -f fastq ${reads}/*.fastq -q
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+NC='\033[0m'
 
-# Trimming procedure (Elimination of low quality sequences at the end of reads)
-# conda install -c bioconda trimmomatic
+# Step 1: Mapping
 
-mkdir -p ${trimming}
-
-for read1_file in ${reads}/*_r1F.fastq
+#### Mapping (BWA) ####
+mkdir ${mapping} -p
+for read1_file in ${trimming}/*1P.fastq
 do
-	paired_file_with_path=${read1_file%_r1F.fastq};
-	paired_file_without_path=${paired_file_with_path#${reads}/};
-	echo "Trimming ${paired_file_without_path%.sampled}...";
-    echo ${paired_file_with_path}_r1F.fastq
-	trimmomatic PE ${paired_file_with_path}_r1F.fastq ${paired_file_with_path}_r2F.fastq -baseout ${trimming}/${paired_file_without_path}.fastq LEADING:20 TRAILING:20 MINLEN:50 -quiet
-	echo "Done."
+	paired_file_with_path=${read1_file%_1P.fastq};
+	paired_file_without_path=${paired_file_with_path#${trimming}/};
+	echo -e "\n${BLUE}Downloading SAM file with ${paired_file_without_path}...${NC}\n";
+bwa mem -M -t ${nb_cpus_mapping} -A 2 -E 1 \
+    ${index}/chr16.fa.gz \
+    ${paired_file_with_path}_1P.fastq \
+    ${paired_file_with_path}_1P.fastq \
+    -o ${mapping}/${paired_file_without_path}.sam;
+    echo -e "${GREEN}Done.${NC}";
 done
 
-# Removal of the bases from the extremity with a quality lower than 20. If the final read is smaller than 50, it is discarded. file with U => discard. file with P => no discard.
-# Remark: files 1U and 2U returns a small number of sequences (around 10 000) while files 1P and 2P returns a large number of sequences (a little smaller than the reads without cleaning)
-
-mkdir -p ${figures_trimming}
-echo "Creation of the fastqc files on trimmed reads."
-fastqc -o ${figures_trimming} -f fastq ${trimming}/*.fastq -q
-
+# Options used:
+# -t INT        Number of threads 
+# -A INT        Matching score. 
+# -E INT        Gap extension penalty. A gap of length k costs O + k*E (i.e. -O is for opening a zero-length gap).
+# -M            Mark shorter split hits as secondary (for Picard compatibility)
