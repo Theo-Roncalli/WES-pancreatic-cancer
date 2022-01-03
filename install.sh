e4112bab598d0fecb240623b9703e41f7dc2583b
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-03 18:23:52 +0100

    :art: Modification of the output message color

--- install.sh
+++ install.sh
@@ -15,23 +15,26 @@ genome_url=http://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes/chr16.fa.g
 
 # Color parameters
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
 
 # Step 1: Download reads
 
 mkdir -p ${reads}
-echo "Downloading reads..."
+echo -e "\n${BLUE}Downloading reads...${NC}"
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1DM9g8OulE1ScBk-HoaREfUZs6gurtkBe" \
     -O ${reads}/patient7.tar.gz && rm -rf /tmp/cookies.txt # The options in the command wget allow to download a large file on Google Drive.
-echo "Unarchiving reads..."
+echo -e "${GREEN}Done.${NC}"
+echo -e "\n${BLUE}Unarchiving reads...${NC}"
 tar -zxf ${reads}/patient7.tar.gz -C ${reads}
+echo -e "${GREEN}Done.${NC}"
 mv ${reads}/patient7.exome/* ${reads} && rm -r ${reads}/patient7.exome && rm ${reads}/patient7.tar.gz # Cleaning 'reads' repository
 gunzip -q ${reads}/*.fastq.gz
 
 # Print read numbers and file sizes for each sample
-echo "--------Number of reads and size of each file--------"
+echo -e "\n${BLUE}-------------Number of reads and size of each file-------------${NC}"
 for file in ${reads}/*.fastq
 do
     # Print filename, number of reads and size of a specific file
@@ -57,9 +60,9 @@ echo -e "\n"
 # Step 2: Quality control + Reads cleaning
 
 mkdir -p ${figures_reads}
-echo "Creation of the fastqc files on raw reads."
+echo -e "${BLUE}Creation of the fastqc files on raw reads.${NC}"
 fastqc -o ${figures_reads} -f fastq ${reads}/*.fastq -q
-echo "Done."
+echo -e "${GREEN}Done.${NC}"
 
 # Trimming procedure (Elimination of low quality sequences at the end of reads)
 # conda install -c bioconda trimmomatic
@@ -70,21 +73,31 @@ for read1_file in ${reads}/*_r1F.fastq
 do
 	paired_file_with_path=${read1_file%_r1F.fastq};
 	paired_file_without_path=${paired_file_with_path#${reads}/};
-	echo "Trimming ${paired_file_without_path%.sampled}...";
+	echo -e "\n${BLUE}Trimming ${paired_file_without_path%.sampled}...${NC}";
     echo ${paired_file_with_path}_r1F.fastq
 	trimmomatic PE ${paired_file_with_path}_r1F.fastq ${paired_file_with_path}_r2F.fastq -baseout ${trimming}/${paired_file_without_path}.fastq LEADING:20 TRAILING:20 MINLEN:50 -quiet
-	echo "Done."
+echo -e "${GREEN}Done.${NC}"
 done
+
+# Removal of the bases from the extremity with a quality lower than 20. If the final read is smaller than 50, it is discarded. file with U => discard. file with P => no discard.
+# Remark: files 1U and 2U returns a small number of sequences (around 10 000) while files 1P and 2P returns a large number of sequences (a little smaller than the reads without cleaning)
+
+mkdir -p ${figures_trimming}
+echo -e "\n${BLUE}Creation of the fastqc files on trimmed reads.${NC}"
+fastqc -o ${figures_trimming} -f fastq ${trimming}/*.fastq -q
+echo -e "${GREEN}Done.${NC}"
 
 # Step 3: Download reference genome
 
 mkdir ${index} -p
-echo "Downloading genome..."
+echo -e "\n${BLUE}Downloading genome...${NC}"
 wget ${genome_url} -P ${index} -q
+echo -e "${GREEN}Done.${NC}"
 
 # Step 4: Creation of the index
 
 #### Index (BWA) ####
 mkdir ${index} -p
+echo -e "\n${BLUE}Creation of the BWA index...${NC}"
 bwa index -a bwtsw ${index}/chr16.fa.gz
-# mv ${genome}/*.fa.gz.+([A-Za-z]) ${index}/
+echo -e "${GREEN}Done.${NC}"
