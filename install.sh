a4c1f3f46f0c2891b8843a9ff8da7acf732154e8
Théo Roncalli <theo.roncalli@yahoo.fr>
2022-01-04 15:35:23 +0100

    :recycle: Refactor code

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ genome_url=http://hgdownload.soe.ucsc.edu/goldenPath/hg19/chromosomes/chr16.fa.g
 
 RED='\033[1;31m'
 GREEN='\033[1;32m'
-BLUE='\033[0;34m'
+BLUE='\033[1;34m'
 NC='\033[0m'
 
 # Step 1: Download reads
@@ -60,7 +60,7 @@ echo -e "\n"
 # Step 2: Quality control + Reads cleaning
 
 mkdir -p ${figures_reads}
-echo -e "${BLUE}Creation of the fastqc files on raw reads.${NC}"
+echo -e "${BLUE}Creation of the fastqc files on raw reads...${NC}"
 fastqc -o ${figures_reads} -f fastq ${reads}/*.fastq -q
 echo -e "${GREEN}Done.${NC}"
 
@@ -83,7 +83,7 @@ done
 # Remark: files 1U and 2U returns a small number of sequences (around 10 000) while files 1P and 2P returns a large number of sequences (a little smaller than the reads without cleaning)
 
 mkdir -p ${figures_trimming}
-echo -e "\n${BLUE}Creation of the fastqc files on trimmed reads.${NC}"
+echo -e "\n${BLUE}Creation of the fastqc files on trimmed reads...${NC}"
 fastqc -o ${figures_trimming} -f fastq ${trimming}/*.fastq -q
 echo -e "${GREEN}Done.${NC}"
 
@@ -97,7 +97,10 @@ echo -e "${GREEN}Done.${NC}"
 # Step 4: Creation of the index
 
 #### Index (BWA) ####
+# sudo apt install bwa
 mkdir ${index} -p
 echo -e "\n${BLUE}Creation of the BWA index...${NC}"
 bwa index -a bwtsw ${index}/chr16.fa.gz
 echo -e "${GREEN}Done.${NC}"
+
+exit
